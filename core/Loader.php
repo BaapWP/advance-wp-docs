be8bf0e28345553adfb0bf6b4c6f38dcefabceb7
Saurabh Shukla <saurabh@yapapaya.com>
2018-06-22 00:39:16 +0530

    Document and fix global

--- core/Loader.php
+++ core/Loader.php
@@ -1,21 +1,34 @@
 <?php
-
+/**
+ * Contains the Core Loader
+ *
+ * @since 1.0.0
+ */
 namespace BWP\AdvanceDocs\Core;
 
+/**
+ * Loads the plugin
+ */
 class Loader {
 
+	/**
+	 * Hooks into WordPress
+	 */
 	public function hook() {
 		add_action( 'init', array( $this, 'init' ) );
 	}
 
+	/**
+	 * Initilaise plugin
+	 */
 	public function init() {
 
-		$_GLOBALS['wp_advance_docs'] = array(
+		$GLOBALS['wp_advance_docs'] = array(
 			'objects' => null,
 		);
 
+		// Register Custom Content Types
 		$register_objects = new \Core\Register_Objects;
-
 		$register_objects->init();
 	}
 
